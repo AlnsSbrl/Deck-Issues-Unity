95597d5c5691d9baf4fe0d763c7021fd4b0fefc2
AlnsSbrl <106735439+AlnsSbrl@users.noreply.github.com>
2025-03-30 19:59:13 +0200

    deleted tests
    deleted tests

--- Deck Issues/Assets/Tests/Tests/CardDealerTest.cs
+++ Deck Issues/Assets/Tests/Tests/CardDealerTest.cs
@@ -18,19 +18,11 @@ public class CardDealerTest
     [Test]
     public void AssertAllCardsAreAvailableBeforeDealingTheCards()
     {
-        //given
-        var cards = Enum.GetValues(typeof(SpanishDeck));
-        //when
-        CurreloGameController dealer = new CurreloGameController();
-        dealer.InitCards(Deck.Spanish);
-        //then
-        TestContext.WriteLine($"Expected {cards.Length} but got {dealer.PossibleCards.Count}");
-        Assert.AreEqual(cards.Length,dealer.PossibleCards.Count, $"Expected {cards.Length} but got {dealer.PossibleCards.Count}");
+        
     }
     [Test]
     public void AssertAllAvailableCardsAreDifferent()
     {
-
 
     }
 }
